56a6beab87c3f92f18afb14a8f13d41d3066555e
mcorekci <mustafa.corekci@dzsi.com>
2024-09-07 19:42:22 +0300

    config server is implemented to manage all the configurations

---
+++ config-server/src/test/java/com/emce/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emce.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
