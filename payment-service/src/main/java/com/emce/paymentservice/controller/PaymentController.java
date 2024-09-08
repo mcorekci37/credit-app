152b71214022a156d3eb1ec10475007ed6caec44
mcorekci <mustafa.corekci@dzsi.com>
2024-09-09 02:17:54 +0300

    payment service is created and implemented some basic logics

---
+++ payment-service/src/main/java/com/emce/paymentservice/controller/PaymentController.java
@@ -0,0 +1,25 @@
+package com.emce.paymentservice.controller;
+
+import com.emce.paymentservice.dto.CreditResponse;
+import com.emce.paymentservice.dto.PaymentRequest;
+import com.emce.paymentservice.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/payment")
+public class PaymentController {
+    private final PaymentService paymentService;
+
+    @PostMapping("/pay")
+    public ResponseEntity<CreditResponse> payInstallment(@RequestBody PaymentRequest paymentRequest){
+        return ResponseEntity.ok(paymentService.pay(paymentRequest));
+    }
+
+
+}
---
+++ payment-service/src/main/java/com/emce/paymentservice/service/PaymentService.java
@@ -0,0 +1,41 @@
+package com.emce.paymentservice.service;
+
+import com.emce.paymentservice.config.CreditClient;
+import com.emce.paymentservice.dto.CreditResponse;
+import com.emce.paymentservice.dto.PaymentRequest;
+import com.emce.paymentservice.entity.Installment;
+import com.emce.paymentservice.entity.InstallmentStatus;
+import com.emce.paymentservice.exception.InstallmentNotFoundException;
+import com.emce.paymentservice.repository.InstallmentRepository;
+import jakarta.ws.rs.NotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentService {
+
+    private final InstallmentRepository repository;
+    private final CreditClient creditClient;
+
+    @Transactional
+    public CreditResponse pay(PaymentRequest request) throws NotFoundException {
+        Installment installment = repository.findById(request.installmentId()).orElseThrow(
+                () -> new InstallmentNotFoundException(String.format("Installment with %s id not found", request.installmentId())));
+        if (installment.getDept().equals(request.amount())) {
+            installment.setStatus(InstallmentStatus.AMORTIZED);
+            installment.setDept(0D);
+        } else if (Double.compare(installment.getDept(),request.amount())>0) {
+            installment.setStatus(InstallmentStatus.PARTIALLY_AMORTIZED);
+            installment.setDept(installment.getDept()-request.amount());
+        } else if (Double.compare(installment.getDept(),request.amount())<0) {
+            installment.setStatus(InstallmentStatus.AMORTIZED);
+            installment.setDept(0D);
+            //todo trying to pay more than the dept. to be handled carefully
+        }
+        installment = repository.save(installment);
+        CreditResponse credit = creditClient.getCredit(installment.getCredit().getId());
+        return credit;
+    }
+}
