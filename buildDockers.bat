831afa94a851ca41c2657e1c8b979301c66d454c
mcorekci <mustafa.corekci@dzsi.com>
2024-09-11 13:23:56 +0300

    dockerfiles for docker images created

---
+++ buildDockers.bat
@@ -0,0 +1,38 @@
+@echo off
+
+echo Running auth-server/buildDocker.bat
+cd auth-server
+call buildDocker.bat
+cd ..
+
+echo Running config-server/buildDocker.bat
+cd config-server
+call buildDocker.bat
+cd ..
+
+echo Running credits-service/buildDocker.bat
+cd credits-service
+call buildDocker.bat
+cd ..
+
+echo Running discovery-server/buildDocker.bat
+cd discovery-server
+call buildDocker.bat
+cd ..
+
+echo Running gateway/buildDocker.bat
+cd gateway
+call buildDocker.bat
+cd ..
+
+echo Running interest-service/buildDocker.bat
+cd interest-service
+call buildDocker.bat
+cd ..
+
+echo Running payment-service/buildDocker.bat
+cd payment-service
+call buildDocker.bat
+cd ..
+
+pause
